2fdc58f91a05b03ef90987e07dcd8e89b658b9c7
oktay <oktaycolakoglu@gmail.com>
2020-08-16 22:47:10 +0300

    advert link hover

--- src/components/Advert/index.js
+++ src/components/Advert/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Img from 'gatsby-image'
 import { Location } from '../icons'
 
@@ -6,20 +7,22 @@ import styles from './advert.module.css'
 
 const Advert = ({advertData}) => {
   return (
-    <article className={styles.advert}>
-      <div className={styles.hero}>
-        <Img className={styles.heroImage} fluid={advertData.thumbnail.childImageSharp.fluid} />
-        <span className={styles.date}>{advertData.created_at}</span>
-      </div>
-      <h3>{advertData.title}</h3>
-      <div className={styles.footer}>
-        <span className={styles.location}>
-          <Location className={styles.icon} />
-          <span>{advertData.location}</span>
-        </span>
-        <span className={styles.price}>{advertData.price}</span>
-      </div>
-    </article>
+    <Link to={`/${advertData.slug}`}>
+      <article className={styles.advert}>
+        <div className={styles.hero}>
+          <Img className={styles.heroImage} fluid={advertData.thumbnail.childImageSharp.fluid} />
+          <span className={styles.date}>{advertData.created_at}</span>
+        </div>
+        <h3>{advertData.title}</h3>
+        <div className={styles.footer}>
+          <span className={styles.location}>
+            <Location className={styles.icon} />
+            <span>{advertData.location}</span>
+          </span>
+          <span className={styles.price}>{advertData.price}</span>
+        </div>
+      </article>
+    </Link>
   )
 }
 
