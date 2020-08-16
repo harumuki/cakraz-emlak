81b374f8bdda61796d3a9cbcf27f4c73e47a74a9
oktay <oktaycolakoglu@gmail.com>
2020-08-17 00:01:00 +0300

    title component cx

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,9 +3,9 @@ import cx from 'classnames'
 
 import styles from './title.module.css'
 
-const Title = ({ children, tag }) => {
+const Title = ({ children, tag, className }) => {
   const TagName = tag ? tag : 'h1'
-  return <TagName className={cx(TagName === 'h1' && styles.hero)}>{children}</TagName>
+  return <TagName className={cx(TagName === 'h1' && styles.hero, className)}>{children}</TagName>
 }
 
 
