5c83dbb9da3317ac01e7a84ce5b92d2351411c3e
oktay <oktaycolakoglu@gmail.com>
2020-08-16 04:45:06 +0300

    type definations

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,30 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type StrapiAdvert implements Node {
+      id: String
+      slug: String
+      title: String
+      description: String
+      location: String
+      price: String
+      type: String
+      roomcount: String
+      size: String
+      buildage: Int
+      floorcount: String
+      floorcurrent: String
+      bathcount: Int
+      furnished: Boolean
+      incomplex: Boolean
+      heating: String
+      thumbnail: File
+      images: [File]
+    }
+  `
+  createTypes(typeDefs)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,24 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Img from 'gatsby-image'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <ul>
-      {data.allStrapiAdvert.edges.map(advert => (
-        <li key={advert.node.id}>
-          <Img fixed={advert.node.thumbnail.childImageSharp.fixed} width="350" />
-          <span>{advert.node.title}</span>
-        </li>
-      ))}
-    </ul>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const adverts = data.allStrapiAdvert.edges
+  console.log(adverts)
+  return (
+    adverts.length ? (
+      <ul>
+        {adverts.map(advert => (
+          <Link to={advert.node.slug} key={advert.node.id}>
+            <h5>{advert.node.title}</h5>
+            {advert.node.thumbnail ? <Img fixed={advert.node.thumbnail.childImageSharp.fixed}/> : ''}
+            <span>{advert.node.location}</span>
+            <span>{advert.node.price}</span>
+          </Link>
+        ))}
+      </ul>
+    ) : <h1>Hiç ilan yok</h1>
+  )
+}
 
 export const query = graphql`
   {
@@ -36,6 +29,8 @@ export const query = graphql`
           id
           slug
           title
+          location
+          price
           thumbnail {
             childImageSharp {
               fixed(width: 600) {
