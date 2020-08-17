ea0054e42a39e0367f1a02373d44cba5fcbe9b13
oktay <oktay.colakoglu@akinon.com>
2020-08-17 16:48:43 +0300

    images query fix

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,25 +1,27 @@
 import React from 'react'
-import { graphql, StaticQuery, Link } from 'gatsby'
+import { graphql, useStaticQuery, Link } from 'gatsby'
 import Img from 'gatsby-image'
 
 import styles from './logo.module.css'
 
-const Logo = () => (
-  <StaticQuery
-    query={graphql`
-      {
-        file(relativePath: {eq: "logo.png"}) {
-          childImageSharp {
-            fluid {
-              ...GatsbyImageSharpFluid_tracedSVG
-            }
+const Logo = () => {
+  const data = useStaticQuery(graphql`
+    {
+      file(relativePath: { eq: "logo.png" }) {
+        childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
           }
         }
       }
-    `}
-    render={data => <Link to="/"><Img fluid={data.file.childImageSharp.fluid} className={styles.logo} /></Link>}
-  ></StaticQuery>
-)
+    }
+  `)
 
+  return (
+    <Link to="/">
+      <Img fluid={data.file.childImageSharp.fluid} className={styles.logo} />
+    </Link>
+  )
+}
 
 export default Logo
