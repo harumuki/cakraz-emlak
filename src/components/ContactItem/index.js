af848c42ff89f3627cd97e6820a3bea95338bd69
oktay <oktay.colakoglu@akinon.com>
2020-08-17 17:18:28 +0300

    fix href warning

--- src/components/ContactItem/index.js
+++ src/components/ContactItem/index.js
@@ -5,7 +5,7 @@ import styles from './contact-item.module.css'
 
 const ContactItem = ({icon, title, value}) => {
   return (
-    <a href={title === 'Telefon' ? `tel:` + value : title === 'Email' ? `mailto:` + value : '#'}>
+    <a href={title === 'Telefon' ? `tel:` + value : title === 'Email' ? `mailto:` + value : '/#'}>
       <div className={styles.item}>
         {
           icon === 'Phone' ? <Phone className={styles.icon} /> : icon === 'Location' ? <Location className={styles.icon} /> : icon === 'Email' ? <Email className={styles.icon} /> : <div/>
