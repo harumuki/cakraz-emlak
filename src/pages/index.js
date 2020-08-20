c8990d56511ef19c68f94e86fd9e618fa4d79139
oktay <oktaycolakoglu@gmail.com>
2020-08-20 14:46:20 +0300

    index heroimg

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import HeroImg from '../components/HeroImg'
 import Highlights from '../components/Highlights'
 
-const IndexPage = () => {
+const IndexPage = ({data}) => {
   return (
     <Layout>
+      <HeroImg image={data.homepage.heroimg.localFile.childImageSharp.fluid} height={750} />
       <Highlights />
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    homepage: strapiHomepage {
+      heroimg {
+        localFile {
+          childImageSharp {
+            fluid(maxWidth: 1920) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
