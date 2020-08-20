fcbecccca02e4f520a67d4b1d49bf0d66252cac9
oktay <oktaycolakoglu@gmail.com>
2020-08-20 16:49:57 +0300

    header z-index

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import cx from 'classnames'
-import useWindowSize from '../../hooks/useWindowSize';
+import useWindowSize from '../../hooks/useWindowSize'
 import { Link, useStaticQuery, graphql } from 'gatsby'
 import { Hamburger, Times } from '../icons'
 
@@ -9,7 +9,7 @@ import styles from './navigation.module.css'
 const Navigation = () => {
   const [toggle, setToggle] = useState(false)
 
-  const size = useWindowSize();
+  const size = useWindowSize()
 
   const data = useStaticQuery(graphql`
     {
@@ -26,25 +26,33 @@ const Navigation = () => {
 
   return (
     <>
-    {size.width <= 960 && toggle && <div className={styles.backdrop}></div>}
+      {size.width <= 960 && (
+        <div className={styles.icon}>
+          {!toggle && (
+            <Hamburger onClick={() => setToggle(true)} />
+          )}
+          {toggle && (
+            <Times onClick={() => setToggle(false)} />
+          )}
+        </div>
+      )}
+      <nav
+        className={cx(
+          styles.navigation,
+          size.width <= 960 && !toggle && styles.hidden
+        )}
+      >
+        <Link to="/">Anasayfa</Link>
+        <Link to="/satilik">Satılık</Link>
+        <Link to="/kiralik">Kiralık</Link>
 
-    <div className={styles.icon}>
-      {size.width <= 960 && !toggle && <Hamburger onClick={() => setToggle(true)} />}
-      {size.width <= 960 && toggle && <Times onClick={() => setToggle(false)} />}
-    </div>
-
-    <nav className={cx(styles.navigation, size.width <= 960 && !toggle && styles.hidden)}>
-      <Link to="/">Anasayfa</Link>
-      <Link to="/satilik">Satılık</Link>
-      <Link to="/kiralik">Kiralık</Link>
-
-      {data.allStrapiPage.edges.length > 0 &&
-        data.allStrapiPage.edges.map(page => (
-          <Link key={page.node.slug} to={`/${page.node.slug}`}>
-            {page.node.title}
-          </Link>
-        ))}
-    </nav>
+        {data.allStrapiPage.edges.length > 0 &&
+          data.allStrapiPage.edges.map(page => (
+            <Link key={page.node.slug} to={`/${page.node.slug}`}>
+              {page.node.title}
+            </Link>
+          ))}
+      </nav>
     </>
   )
 }
