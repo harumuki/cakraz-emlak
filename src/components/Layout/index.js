bb53347ca69d7a2f16d794f2ee3a760bc5d7675a
oktay <oktaycolakoglu@gmail.com>
2020-08-26 13:33:27 +0300

    seo fields

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -4,11 +4,22 @@ import Header from '../Header'
 import Footer from '../Footer'
 
 const Layout = ({ children, title }) => {
+  const data = useStaticQuery(graphql`
+    {
+      strapiHomepage {
+        title
+        description
+        keywords
+      }
+    }
+  `)
   return (
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title ? title + ' | ' : ''}Çakraz Emlak</title>
+        <title>{title ? title + ' | ' : ''}{data.strapiHomepage.title}</title>
+        <meta name="description" content={data.strapiHomepage.description} />
+        <meta name="keywords" content={data.strapiHomepage.keywords} />
       </Helmet>
       <Header />
       <main>{children}</main>
