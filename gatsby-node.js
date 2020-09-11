93f3fdad98dfa33f7eb7c7769f5e93218ae89c75
oktay <oktaycolakoglu@gmail.com>
2020-09-11 11:06:55 +0300

    refactor: gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,35 +1,15 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
-
-const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
-const path = require(`path`)
-
-const makeRequest = (graphql, request) =>
-  new Promise((resolve, reject) => {
-    resolve(
-      graphql(request).then(result => {
-        if (result.errors) {
-          reject(result.errors)
-        }
-
-        return result
-      })
-    )
-  })
-
-exports.createPages = ({ actions, graphql }) => {
-  const { createPage } = actions
-
-  const getAdverts = makeRequest(
-    graphql,
-    `
+exports.createPages = async ({ graphql, actions }) => {
+  const result = await graphql(`
     {
-      allStrapiAdvert(filter: {published: {eq: true}}) {
+      allStrapiAdvert(filter: { published: { eq: true } }) {
+        edges {
+          node {
+            id
+            slug
+          }
+        }
+      }
+      allStrapiPage(filter: { published: { eq: true } }) {
         edges {
           node {
             id
@@ -38,46 +18,35 @@ exports.createPages = ({ actions, graphql }) => {
         }
       }
     }
-    `
-  ).then(result => {
-    result.data.allStrapiAdvert.edges.forEach(({ node }) => {
-      createPage({
-        path: `/${node.slug}`,
-        component: path.resolve(`src/templates/advert.js`),
-        context: {
-          id: node.id,
-          slug: node.slug,
-        },
-      })
-    })
-  })
+  `)
 
-  const getPages = makeRequest(
-    graphql,
-    `
-    {
-      allStrapiPage(filter: {published: {eq: true}}) {
-        edges {
-          node {
-            id
-            slug
-          }
-        }
-      }
+  const adverts = result.data.allStrapiAdvert.edges
+
+  adverts.forEach(advert => {
+    if (!advert || !advert.node.id || !advert.node.slug) {
+      return
     }
-    `
-  ).then(result => {
-    result.data.allStrapiPage.edges.forEach(({ node }) => {
-      createPage({
-        path: `/${node.slug}`,
-        component: path.resolve(`src/templates/page.js`),
-        context: {
-          slug: node.slug,
-          id: node.id,
-        },
-      })
+
+    actions.createPage({
+      path: `/${advert.node.slug}`,
+      component: require.resolve('./src/templates/advert.js'),
+      context: {
+        id: advert.node.id,
+        slug: advert.node.slug,
+      },
     })
   })
 
-  return Promise.all([getAdverts, getPages])
+  const pages = result.data.allStrapiPage.edges
+
+  pages.forEach(page => {
+    actions.createPage({
+      path: `/${page.node.slug}`,
+      component: require.resolve('./src/templates/page.js'),
+      context: {
+        id: page.node.id,
+        slug: page.node.slug,
+      },
+    })
+  })
 }
